799caa4f2479175c6bfff98864bf2ba83c88715b
BuGalter <wardoc78@gmail.com>
2021-02-14 17:17:23 +0300

    Added license to project and in init.py

--- bot_carcass/__init__.py
+++ bot_carcass/__init__.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# Copyright [2021] [Valeriy Yukubchik]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Пакет содержит каркас простого чат-бота для телеграм
 
 bot.py : Основной, запускаемый модуль, содержит код бота.
@@ -11,5 +26,5 @@ urls.py : Модуль содержит все урлы. функции для �
 
 Valera WarDoc [https://github.com/BuGalter]
 wardoc78@gmail.com
-# License:
+# License: Apache License 2.0
 version: "1.0.0"
