a9204eaa01cd9e8e47a9f5aa02145990ce3d45ce
BuGalter <wardoc78@gmail.com>
2021-02-14 17:18:54 +0300

    Added package tests and description to init.py for tests

---
+++ tests/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Copyright [2021] [Valeriy Yukubchik]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Пакет содержит тесты
+
+bot.py : Основной, запускаемый модуль, содержит код бота.
+
+commands.py : Модуль содержит команды, которые знает бот и ответы.
+Также ответ, если команда не известна.
+
+urls.py : Модуль содержит все урлы. функции для получения токена и
+базового урла. Также функции для получения урлов, которые 
+необходимы для работы бота.
+
+Valera WarDoc [https://github.com/BuGalter]
+wardoc78@gmail.com
+# License: Apache License 2.0
+version: "1.0.0"
