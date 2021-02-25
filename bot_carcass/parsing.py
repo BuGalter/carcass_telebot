52541949ee7e0588324904b09b8ed8f5a459449d
BuGalter <wardoc78@gmail.com>
2021-02-25 20:23:41 +0300

    Added some discription to function get_status_message

--- bot_carcass/parsing.py
+++ bot_carcass/parsing.py
@@ -136,6 +136,29 @@ def get_text_update(update: dict, key_update: str) -> str:
     return update[key_update]['text']
 
 
+def get_message_status(update: dict) -> str:
+    """функция для получения статуса сообщения от бота.
+
+    Описание - функция получает статус сообщения, сортируя
+    ключи и выбирая первый элемент, полученого словаря, который
+    содержит сообщение от бота
+
+    Parameters
+    ----------
+        update : dict
+            новое сообщение от бота
+
+    Returns
+    -------
+        message_status : str
+            статус сообщения, если новое, то message, если отредактированое
+            edited_message
+
+    """
+    message_status = sorted(update.keys())[0]
+    return message_status
+
+
 def parsing_text_update(text: str) -> str:
     """функция для проверки текста на совпадение с командами бота.
 
@@ -157,26 +180,3 @@ def parsing_text_update(text: str) -> str:
     if text in BOT_COMMANDS.keys():
         return BOT_COMMANDS[text]
     return BOT_COMMANDS['not_command']
-
-
-def get_message_status(update: dict) -> str:
-    """функция для получения статуса сообщения от бота.
-
-    Описание - функция получает статус сообщения, сортируя
-    ключи и выбирая первый элемент, полученого словаря, который 
-    содержит сообщение от бота
-
-    Parameters
-    ----------
-        update : dict
-            новое сообщение от бота
-
-    Returns
-    -------
-        message_status : str
-            статус сообщения, если новое, то message, если отредактированое
-            edited_message
-
-    """
-    message_status = sorted(update.keys())[0]
-    return message_status
