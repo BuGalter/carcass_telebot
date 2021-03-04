47f048e653a2d96c152db802d05aa7f06ceab359
BuGalter <wardoc78@gmail.com>
2021-03-04 17:50:09 +0300

    Added descriptions to test functions in test_urls.py

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -19,39 +19,78 @@
 
 Functions
 ---------
-tes_get_token()
+test_get_token()
     Тесты для функции get_token, получены корректные данные
 
-tes_get_token_none()
+test_get_token_none()
     Тесты для функции get_token, когда указан неверный ключ
 
-tes_get_token_absent()
+test_get_token_absent()
     Тесты для функции get_token, файл не содержит данные
+test_get_base_url()
+    тесты для функции get_base_url
+test_get_method_url()
+    набор тестов для функции get_method_url
+test_get_method_url_none()
+    тест для функции get_method_urlесли имя метода отсутствует
+    среди методов бота
 """
 from bot_carcass import urls
 
 
 def test_get_token():
+    """Тест для корректных данных.
+
+    Описание - тест, когда в файле содержится переменная TOKEN,
+    которая содержит коректный токен для апи телеграм.
+    """
     result = urls.get_token(".env1")
     assert result == 'FFFFFFF'
 
 
 def test_get_token_none():
+    """Тест для корректных данных, но не верное имя переменной.
+
+    Описание - тест, когда в файле содержится любая другая пременная,
+    а не TOKEN.
+    """
     result = urls.get_token('.env2')
     assert result is None
 
 
 def test_get_token_absent():
+    """Тест для пустого файла.
+
+    Описание - тест, когда файл пуст.
+    """
     result = urls.get_token('.env3')
     assert result is None
 
 
 def test_get_base_url():
+    """Тест функции get_base_url().
+
+    Описание - тест для подтверждения корретной работы функции.
+    """
     result = urls.get_base_url('allokey')
     assert result == 'https://api.telegram.org/botallokey/'
 
 
 def test_get_method_url():
+    """Тест функции get_method_url, когда передаются существующие
+    методы.
+
+    Описание - тест для подтверждения корректной работы, если передаются
+    имена методов, которые использует бот.
+
+    Variables
+    ---------
+    url : str
+        произвольный урл
+
+    methods : dict
+        набор методов, которые определенны для бота
+    """
     url = 'https://api.telegram.org/'
     methods = {'get_updates': 'get_updates',
                'send_message': 'send_message',
@@ -65,5 +104,11 @@ def test_get_method_url():
 
 
 def test_get_method_url_none():
+    """Тест функции get_method_url, когда передаются не существующие
+    методы.
+
+    Описание - тест для подтверждения корректной работы, если передаются
+    имена методов, которые бот не знает.
+    """
     result = urls.get_method_url('https://api.telegram.org/', 'none')
     assert result is None
